709a607a9e41167f82e4344daf06bd56bf649a56
shohei.tada <shouhei.tada@smartstudy.co.jp>
2020-05-22 09:16:19 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -50,6 +50,7 @@ class TasksController extends Controller
     {
         // メッセージを作成
         $task = new Task;
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
@@ -103,6 +104,7 @@ class TasksController extends Controller
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // タスクを更新
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
